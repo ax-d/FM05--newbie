7cded520799455e51c94d39f56467dbef5729585
Anonymous X <info@anonymous-x.nl>
2020-06-09 10:25:08 +0200

    desktop_commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.content__form');
+const formItem = document.querySelector('.content-form__input');
+const formError = document.querySelector('.form-error');
+
+form.setAttribute('novalidate', true);
+
+formItem.addEventListener('blur', function (event) {
+    if (formItem.validity.valid)
+        formError.textContent = '';
+    else
+        showError();
+}, true);
+
+form.addEventListener('submit', function (event) {
+    if (!formItem.validity.valid) {
+        showError();
+
+        event.preventDefault();
+    }
+});
+
+function showError() {
+    formItem.style.borderColor = 'hsl(354, 100%, 66%)';
+
+    if (formItem.validity.valueMissing)
+        formError.textContent = "Whoops! It looks like you forgot to add your email";
+    else if (formItem.validity.patternMismatch)
+        formError.textContent = "Please provide a valid email address";
+}
